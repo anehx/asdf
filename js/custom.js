14775e08452e80a52f4c1aa6e5698eb415eafb13
Manuel Schenk <manuel.schenk@evosys.ch>
2017-03-23 14:20:33 +0100

    Adds smooth scrolling

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,9 @@
-console.log('main.js initialized');
+$(document).ready(function() {
+    $(document).on('click', '.nav-link', function(event) {
+        event.preventDefault();
+        var target = "#" + this.getAttribute('data-target');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top - 90
+        }, 500);
+    });
+});
