91325f44d5eed571d92aa94ec3150860d99c26d3
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-06-09 14:39:39 +0200

    plyr and cleanup

--- js/creative.js
+++ js/creative.js
@@ -1,39 +1,43 @@
-(function($) {
-    "use strict"; // Start of use strict
+(function() {
+  "use strict"
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(document).on('click', 'a.page-scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+  $(document).on('click', 'a.page-scroll', function(event) {
+    var $anchor = $(this)
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo')
+    event.preventDefault()
+  })
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
+  // Highlight the top nav as scrolling occurs
+  $('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: 51
+  })
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click()
+  })
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+  // Offset for Main Navigation
+  $('#mainNav').affix({
+    offset: {
+      top: 100
+    }
+  })
 
-    $('.modal').on('shown.bs.modal', function() {
-        var target = $(this).find('.cocoen')
+  // Image side by side
+  $('.modal').on('shown.bs.modal', function() {
+    var target = $(this).find('.cocoen')
 
-        if (!target.find('.cocoen-drag').length) {
-            target.addClass('show-images')
-            target.cocoen();
-        }
-    })
-})(jQuery); // End of use strict
+    if (!target.find('.cocoen-drag').length) {
+      target.addClass('show-images')
+      target.cocoen()
+    }
+  })
+
+  // HTML5 player
+  plyr.setup()
+})()
