2ebb6f801cd078739abe727e5e6719e9cf883474
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-06-09 11:18:09 +0200

    Clear

--- README.md
+++
@@ -1,6 +0,0 @@
-# Asdf [![License](https://img.shields.io/github/license/anehx/asdf.svg)]()
-
-School project for module 152
-
-## Styleguide
-See our [styleguide](STYLEGUIDE.md).
--- js/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(document).on('click', '.nav-link', function(event) {
-        event.preventDefault();
-        var target = "#" + this.getAttribute('data-target');
-        $('html, body').animate({
-            scrollTop: $(target).offset().top - 90
-        }, 500);
-    });
-});
